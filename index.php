0213ece4653dad8f59d89de6bb3e1a1a354f2f38
Soucius <soucius.ggwp@gmail.com>
2024-08-18 18:46:50 +0300

    SAVE POINT

--- index.php
+++ index.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        $books = [
-            [
-                "name" => "Do Androids Dream of Electric Sheep",
-                "author" => "Philip K. Dick",
-                "purchaseUrl" => "https://google.com"
-            ],
-            [
-                "name" => "Project Hail Mary",
-                "author" => "Andy Weir",
-                "purchaseUrl" => "https://google.com"
-            ]
-        ];
-    ?>
+<?php
 
-    <ul>
-        <?php foreach ($books as $book) : ?>
-            <li><a href=<?= $book['purchaseUrl'] ?> target="_blank"><?= $book["name"]; ?></a></li>
-        <?php endforeach; ?>
-    </ul>
-</body>
-</html>
+    require "functions.php";
+
+    $heading = "Home";
+
+    require "views/index.view.php";
